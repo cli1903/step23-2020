7fa43937ff4df6f43fc313ffd58bf35a657b6cf3
Anthony Feenster <afeenster@google.com>
2020-06-26 21:11:28 +0000

    Adding a map and functionality to the button.

---
+++ frontend/src/main/webapp/create_trip.js
@@ -0,0 +1,6 @@
+function createMap() {
+  const theMap = document.getElementById('the-map');
+  theMap.setAttribute(
+      'src',
+      'https://www.google.com/maps/embed/v1/place?q=New+York,NY&amp;key=AIzaSyBcqOf_ALEoMMWuPaDY0GEfeT6P90Jx1Ro');
+}
