05a5d03cc030754889befc64a6720c665698de74
Anthony Feenster <afeenster@google.com>
2020-06-26 21:31:30 +0000

    Using a working link for the function.

--- frontend/src/main/webapp/create_trip.js
+++ frontend/src/main/webapp/create_trip.js
@@ -2,5 +2,5 @@ function createMap() {
   const theMap = document.getElementById('the-map');
   theMap.setAttribute(
       'src',
-      'https://www.google.com/maps/embed/v1/place?q=New+York,NY&amp;key=AIzaSyBcqOf_ALEoMMWuPaDY0GEfeT6P90Jx1Ro');
+      'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJOwg_06VPwokRYv534QaPC8g&key=AIzaSyBcqOf_ALEoMMWuPaDY0GEfeT6P90Jx1Ro');
 }
