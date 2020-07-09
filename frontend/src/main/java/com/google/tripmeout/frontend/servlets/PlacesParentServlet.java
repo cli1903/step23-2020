5c70347c7904b561ba3a3ee4633ea9e682fc33b4
Cindy Li <cindy_li@brown.edu>
2020-07-09 17:11:34 +0000

    write doPost

--- frontend/src/main/java/com/google/tripmeout/frontend/servlets/PlacesParentServlet.java
+++ frontend/src/main/java/com/google/tripmeout/frontend/servlets/PlacesParentServlet.java
@@ -2,6 +2,8 @@ package com.google.tripmeout.frontend.servlets;
 
 import com.google.tripmeout.frontend.PlaceVisitModel;
 import com.google.tripmeout.frontend.storage.PlaceVisitStorage;
+import com.google.tripmeout.frontend.TripModel;
+import com.google.tripmeout.frontend.storage.TripStorage;
 import com.google.gson.Gson;
 import com.google.maps.GeoApiContext;
 import com.google.maps.NearbySearchRequest;
@@ -22,14 +24,17 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class PlacesParentServlet extends HttpServlet {
-  private final PlaceVisitStorage storage;
+  private final PlaceVisitStorage placeStorage;
+  private final TripStorage tripStorage;
   private final Gson gson;
 
   private static final String RADIUS_REQUEST_PARAM = "radius"; 
   private static final String SORT_REQUEST_PARAM = "sort";
-  private static final String LATITUDE_REQUEST_PARAM = "latitude";
-  private static final String LONGITUDE_REQUEST_PARAM = "longitude";
+  private static final String TRIPID_REQUEST_PARAM = "tripId"
   private static final String NEXT_PAGE_REQUEST_PARAM = "next-page"
+  private static final String PLACE_NAME_REQUEST_PARAM = "place-name";
+  private static final String PLACEID_REQUEST_PARAM = "placeId";
+  private static final String USER_MARK_REQUEST_PARAM = "user-mark";
 
   private final GeoApiContext CONTEXT = new GeoApiContext.Builder()
     .apiKey("AIzaSyCJ5nD1n3osPyQHjdY1bCE6i887N32UTLM")
@@ -37,37 +42,49 @@ public class PlacesParentServlet extends HttpServlet {
 
   private static final String NEXT_PAGE_TOKEN;
 
-  public PlacesParentServlet(PlaceVisitStorage storage, Gson gson) {
-    this.storage = storage;
+  public PlacesParentServlet(PlaceVisitStorage placeStorage, TripStorage tripStorage, Gson gson) {
+    this.placeStorage = placeStorage;
+    this.tripStorage = tripStorage;
     this.gson = gson;
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String tripId = request.getParameter(TRIPID_REQUEST_PARAM);
+    String placeId = request.getParameter(PLACEID_REQUEST_PARAM);
+    PlaceVisitModel.UserMark status = request.getParameter(USER_MARK_REQUEST_PARAM);
+
+    PlaceDetailsRequest place = new PlaceDetailsRequest(CONTEXT).placeId(placeId);
+    PlaceDetails details = place.await().getResult();
+
+    PlaceVisitModel newPlace = new PlaceVisitModel.builder()
+      .setTripId(tripId)
+      .setPlaceId(placeId)
+      .setName(details.name)
+      .setLocationLat(details.geometry.location.lat)
+      .setLocationLong(details.geometry.location.lng)
+      .build();
+
+    placeStorage.update
+
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
 
-    boolean getNextPage = request.getParameter(NEXT_PAGE_TOKEN).equals("True");
-
-    double latitude = Double.valueOf(request.getParameter(LATITUDE_REQUEST_PARAM));
-    double longitude = Double.valueOf(request.getParameter(LONGITUDE_REQUEST_PARAM));
-    LatLng location = new LatLng(latitude, longitude);
-
+    String tripId = request.getParameter(TRIPID_REQUEST_PARAM);
+    String name = request.getParameter(PLACE_NAME_REQUEST_PARAM);
+    boolean getNextPage = request.getParameter(NEXT_PAGE_TOKEN).equals("true");
     String sortMethod = request.getParameter(SORT_REQUEST_PARAM);
     String radiusString = request.getParameter(RADIUS_REQUEST_PARAM);
     int radius;
 
-    try {
-      radius = Integer.parseInt(radiusString);
-    } catch (NumberFormatException e) {
-      System.err.println("Could not convert to int: " + radiusString);
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      response.setContentType("application/json");
-      response.getWriter().println(gson.toJson("Invalid input for radius parameter"));
-      return;
-    }
+    TripModel trip = tripStorage.getTrip(tripId);
+    LatLng location = new LatLng(trip.locationLat(), trip.locationLong());
 
     try {
-      List<PlacesSearchResult> nearbyPlaces = getNearbyPlaces(location, radius, sortMethod);
+      List<PlacesSearchResult> nearbyPlaces = getNearbyPlaces(tripId, getNextPage, location, radius, sortMethod, name);
       response.setContentType("application/json");
       response.getWriter().println(gson.toJson(nearbyPlaces));
     } catch (Exception e) {
@@ -79,7 +96,7 @@ public class PlacesParentServlet extends HttpServlet {
 
   }
 
-  public List<PlacesSearchResult> getNearbyPlaces(LatLng location, int radius, String sortMethod) 
+  public List<PlacesVisitModel> getNearbyPlaces(String tripId, boolean getNextPage, LatLng location, int radius, String sortMethod, String name) 
       throws ApiException, InterruptedException, IOException {
 
     NearbySearchRequest nearby = new NearbySearchRequest(CONTEXT)
@@ -92,8 +109,8 @@ public class PlacesParentServlet extends HttpServlet {
       nearby = nearby.radius(radius);
     }
 
-    if (sortMethod.equals("popular")) {
-      nearby = nearby.rankby(RankBy.PROMINENCE);
+    if (name != null && name != "") {
+      nearby = nearby.name(name);
     }
 
     if (getNextPage) {
@@ -101,16 +118,26 @@ public class PlacesParentServlet extends HttpServlet {
     }
 
     List<PlacesSearchResult> nearbyPlaces = new ArrayList<>();
-
     PlacesSearchResponse response = nearby.await();
+    PlacesSearchResult[] nearbyPlacesResults = response.results;
 
     String currentToken = "first-time";
 
-    PlacesSearchResult[] nearbyPlacesResults = response.results;
-
     for (PlacesSearchResult place: nearbyPlacesResults) {
       if (!place.permanentlyClosed) {
-        nearbyPlaces.add(place);
+        Optional<PlaceVisitModel> nearPlace = placeStorage.get(tripId, place.placeId);
+        if (nearPlace.isPresent()) {
+          nearbyPlaces.add(nearPlace.get());
+        } else {
+          PlaceVisitModel newPlace = PlaceVisitModel.builder()
+            .setTripId(tripId);
+            .setPlaceId(place.placeId)
+            .setName(place.name)
+            .setLocationLat(location.lat)
+            .setLocationLong(location.lng)
+            .build();
+          nearbyPlaces.add(newPlace);
+        }
       }
     }
 
